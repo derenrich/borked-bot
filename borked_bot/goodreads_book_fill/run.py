4d71924e032a6750b32a2c95aed5978fa4628e39
Daniel Erenrich <daniel@erenrich.net>
2020-10-19 22:44:42 -0400

    add book fill / reorder qualifier

--- borked_bot/goodreads_book_fill/run.py
+++ borked_bot/goodreads_book_fill/run.py
@@ -12,41 +12,38 @@ from io import StringIO, BytesIO
 gr_limiter = RateLimiter(max_calls=1, period=1)
 session = get_session()
 
-def get_author(author_id):
+def get_book(book_id):
     with gr_limiter:
         key = CREDENTIALS['goodreads']
         try:
-            url = f"https://www.goodreads.com/author/show.xml?key={key}&id={author_id}"
-            res = session.get(url, timeout=60)
+            url = f"https://www.goodreads.com/book/show.xml?id={book_id}&key={key}"
+            res = session.get(url, timeout=60).content
             if res:
-                return etree.parse(BytesIO(res.content))
+                return etree.parse(BytesIO(res))
             else:
                 return None
-        except:
+        except Exception as e:
+            logging.error('failed to fetch book data', exc_info=e)
             return None
 
-
+        
 STATED_IN = 'P248'
 GR = 'Q2359213'
 RETRIEVED = 'P813'
 
-def make_quals(repo, name, subs):
+def make_quals(repo, name):
     quals = []
     if name:
         named_as_claim = pywikibot.Claim(repo, NAMED_AS, is_qualifier=True)
         named_as_claim.setTarget(name)
         quals.append(named_as_claim)
-    if subs:
-        sub_count_claim = pywikibot.Claim(repo, SUB_COUNT, is_qualifier=True)
-        sub_count_claim.setTarget(make_quantity(int(subs), repo))
-        quals.append(sub_count_claim)
-    quals.append(point_in_time_claim(repo))
+    quals.append(point_in_time_claim(repo))        
     return quals
     
 
 WD = str(pathlib.Path(__file__).parent.absolute())
 
-with open(WD + '/authors.rq', 'r') as query_file:
+with open(WD + '/books.rq', 'r') as query_file:
     QUERY = query_file.read()
 
 wikidata_site = pywikibot.Site("wikidata", "wikidata")
@@ -54,28 +51,21 @@ repo = wikidata_site.data_repository()
 
 generator = pg.WikidataSPARQLPageGenerator(QUERY, site=wikidata_site)
 
-GR_AUTH_ID = 'P2963'
+GR_BOOK_ID = 'P2969'
 NAMED_AS = 'P1810'
-SUB_COUNT = 'P3744'
 
 for g in tqdm(generator):
     d = g.get()
-    gr_authors = get_valid_claims(d, GR_AUTH_ID)
-
-    for author in gr_authors:
-        author_id = author.getTarget()
-        if not author_id:
+    gr_books = get_valid_claims(d, GR_BOOK_ID)
+    for book in gr_books:
+        book_id = book.getTarget()
+        if not book_id:
             continue
-        gr_author = get_author(author_id)
-        if not gr_author:
+        gr_book = get_book(book_id)
+        if not gr_book:
             continue
-        name = gr_author.find("author/name").text
-        fans = gr_author.find("author/fans_count").text
-
-        point_in_time = point_in_time_claim(repo)
-        quals = make_quals(repo, name, fans)
-
-
-        update_qualifiers(repo, author, quals, "update goodreads data")
-
+        title = gr_book.find("book/title")
+        if title is not None and title.text:
+            quals = make_quals(repo, title.text.strip())
+            update_qualifiers(repo, book, quals, "update goodreads book data")
 
