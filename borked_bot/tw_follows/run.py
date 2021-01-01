2d215db0bdcc1573d240b01f110bd35211714415
Daniel Erenrich <daniel@erenrich.net>
2021-01-01 12:38:33 -0500

    add follower count module

---
+++ borked_bot/tw_follows/run.py
@@ -0,0 +1,103 @@
+import pywikibot
+import traceback
+import sys
+from pywikibot import pagegenerators as pg
+import pathlib
+from ratelimiter import RateLimiter
+from ..credentials import CREDENTIALS
+from ..util.util import *
+from ..util.twitter import *
+from ..util.batcher import *
+from tqdm import tqdm
+from dateutil.parser import isoparse
+from datetime import datetime
+import random
+
+# 300 requests per 15-minute window
+twt_limiter = RateLimiter(max_calls=300, period=15 * 60)
+
+# Up to 100 are allowed in a single request
+MAX_USERS_PER_REQ = 100
+USERS_PER_REQ = 75
+
+s = get_session()
+
+NUMBER_OF_WORKS = 'P3740' # statistics.videoCount	
+NUMBER_OF_FOLLOWERS = 'P3744' # statistics.subscriberCount	
+VIEW_COUNT = 'P5436' # statistics.viewCount	
+TWITTER_USERNAME = 'P2002'
+HAS_QUAL = 'P1552'
+VERIFIED = 'Q28378282'
+TWITTER_ID = 'P6552'
+NAMED_AS = 'P1810' # snippet.title	
+START_TIME = 'P580' # snippet.publishedAt
+END_TIME = 'P582'
+POINT_IN_TIME = 'P585'
+MIN_DATA_AGE_DAYS = 14
+MAX_YT_PER_REQ = 50
+FOLLOWERS = 'P8687'
+
+def make_quals(repo, twt_id):
+    quals = []
+    if twt_id:
+        twt_id_claim = pywikibot.Claim(repo, TWITTER_ID, is_qualifier=True)
+        twt_id_claim.setTarget(twt_id)
+        quals.append(twt_id_claim)
+    quals.append(point_in_time_claim(repo))
+    return quals
+
+WD = str(pathlib.Path(__file__).parent.absolute())
+
+with open(WD + '/accounts.rq', 'r') as query_file:
+    QUERY = query_file.read()
+
+wikidata_site = pywikibot.Site("wikidata", "wikidata")
+repo = wikidata_site.data_repository()
+
+generator = pg.WikidataSPARQLPageGenerator(QUERY, site=wikidata_site)
+
+def fetch_batch(items):
+    tw_ids = []
+    for item in items:
+        d = get_item(item)
+        if not d:
+            continue
+        tw_handles = get_valid_claims(d, TWITTER_USERNAME)
+        tw_ids += map(str, sum([get_valid_qualifier_values(t, TWITTER_ID) for t in tw_handles], []))
+    def get_batch_handles(handles):
+        with twt_limiter:
+            res = batch_get_twitter(s, handles, mode='ids', extra_fields=['public_metrics'])
+        return res
+    res = {}
+    results = batcher(tw_ids, get_batch_handles, MAX_USERS_PER_REQ)
+    for _, fetch in results:
+        res.update(dict(fetch))
+    return res
+
+count = 0
+for item, fetch in batcher(tqdm(generator), fetch_batch, USERS_PER_REQ):
+    d = get_item(item)
+    if not d:
+        continue
+    twt_handles = get_valid_claims(d, TWITTER_USERNAME)
+    ends = sum([get_valid_qualifier_values(t, END_TIME) for t in twt_handles], [])
+    twt_ids = list(map(str, sum([get_valid_qualifier_values(t, TWITTER_ID) for t in twt_handles], [])))
+    if len(twt_ids) > 1 or len(ends) > 0:
+        # for now ignore this case
+        continue
+    for twt_id in twt_ids:
+        try:
+            if twt_id in fetch:
+                data = fetch[twt_id]
+                followers = data.get('public_metrics', {}).get('followers_count')
+                if followers and followers > 1000:
+                    follower_quant = make_quantity(followers, repo)
+                    quals = make_quals(repo, twt_id)
+                    add_claim(repo, item, FOLLOWERS, follower_quant, qualifiers=quals, comment="add follower count")
+                    count += 1
+        except ValueError:
+            traceback.print_exception(*sys.exc_info())
+
+print(f"updated {count} entries")
+
+
--- borked_bot/util/twitter.py
+++ borked_bot/util/twitter.py
@@ -2,24 +2,40 @@ from ..credentials import CREDENTIALS
 from .util import retry
 from requests.exceptions import ReadTimeout
 
-ROOT_URL = "https://api.twitter.com/2/users/by"
+HANDLE_ROOT_URL = "https://api.twitter.com/2/users/by"
+ID_ROOT_URL = "https://api.twitter.com/2/users"
 
-@retry(exceptions=[ReadTimeout])
-def batch_get_twitter(session, handles):
+class TwitterError(Exception): pass
+
+@retry(exceptions=[ReadTimeout, TwitterError])
+def batch_get_twitter(session, handles, mode="handles", extra_fields=[]):
     handles = ','.join(handles)
     params = {}
-    params['usernames'] = handles
-    params['user.fields'] = 'created_at,verified'
+    if mode == "handles":
+        params['usernames'] = handles
+    elif mode == "ids":
+        params['ids'] = handles
+    else:
+        raise RuntimeError(f"invalid mode {mode} must be 'handles' or 'ids'")
+    params['user.fields'] = ','.join(['created_at,verified'] + extra_fields)
 
     headers = {}
     headers['Authorization'] = f'Bearer {CREDENTIALS["twitter-bearer"]}'
-    
-    r = session.get(ROOT_URL, params=params, headers=headers, timeout=10)
+
+    root_url = HANDLE_ROOT_URL
+    if mode == "ids":
+        root_url = ID_ROOT_URL
+    r = session.get(root_url, params=params, headers=headers, timeout=10)
     if r:
         res = r.json()
         if 'data' in res:
             out = dict()
             for r in res['data']:
-                out[r['username'].lower()] = r
+                if mode == 'handles':
+                    out[r['username'].lower()] = r
+                elif mode == 'ids':
+                    out[r['id']] = r
             return out
-    return {}
+    else:
+        raise TwitterError(f"Twitter API Error: {r.content}")
+ 
