d75db5098837c9ae12fadaf62d477d2525a8ea2f
Derek Gurchik <602444+gurchik@users.noreply.github.com>
2024-03-16 14:26:36 -0400

    Use PyTwitchAPI

--- borked_bot/util/twitch.py
+++ borked_bot/util/twitch.py
@@ -1,67 +1,20 @@
 from ..credentials import CREDENTIALS
-from .util import *
-from requests.exceptions import ReadTimeout, ConnectionError
-
-MAX_RESULTS = 100
+from twitchAPI.twitch import Twitch
 
 
-class TwitchError(Exception): pass
+async def make_twitch_client():
+    return await Twitch(CREDENTIALS["twitch-client-id"], CREDENTIALS["twitch-secret"])
 
 
-@retry(exceptions=[ConnectionError, ReadTimeout])
-def get_twitch_access_token(session):
-    # https://dev.twitch.tv/docs/authentication/getting-tokens-oauth/#client-credentials-grant-flow
-    params = {
-        "client_id": CREDENTIALS["twitch-client-id"],
-        "client_secret": CREDENTIALS["twitch-secret"],
-        "grant_type": "client_credentials",
-    }
-    r = session.post("https://id.twitch.tv/oauth2/token", data=params)
-    r.raise_for_status()
-    data = r.json()
-    # The response also includes an "expires_in" field, which is the number of seconds
-    # the token is valid for. Technically we get penalized if we are frequently calling
-    # the API with expired tokens. But from my testing these tokens are good for multiple
-    # days at a time, so I won't implement that now.
-    return data["access_token"]
-
-
-@retry(exceptions=[ConnectionError, ReadTimeout])
-def create_twitch_session():
-    s = get_session()
-    s.headers.update({
-        "Client-Id": CREDENTIALS["twitch-client-id"],
-    })
-    token = get_twitch_access_token(s)
-    s.headers.update({
-        "Client-Id": CREDENTIALS["twitch-client-id"],
-        "Authorization": f"Bearer {token}",
-    })
-    return s
-
-
-@retry(exceptions=[ConnectionError, ReadTimeout])
-def batch_get_twitch_ids(session, handles):
-    if len(handles) > MAX_RESULTS:
-        raise TwitchError(f"Cannot lookup more than {MAX_RESULTS} handles")
-    params = {
-        "login": handles, # login=foo&login=bar&login=baz
-    }
-    r = session.get("https://api.twitch.tv/helix/users", params=params)
-    r.raise_for_status()
-    data = r.json()
+async def batch_get_twitch_ids(twitch, handles):
+    # There is a limit of 100 users at one time. I'm assuming the client will give a
+    # good descriptive exception when we exceed that, otherwise we should add a check.
+    users = twitch.get_users(logins=handles)
     return {
-        user["login"]: user["id"]
-        for user in data
+        user.login: user.id
+        async for user in users
     }
 
-
-@retry(exceptions=[ConnectionError, ReadTimeout])
-def get_twitch_follows(session, user_id):
-    params = {
-        "broadcaster_id": user_id,
-    }
-    r = session.get("https://api.twitch.tv/helix/channels/followers", params=params)
-    r.raise_for_status()
-    data = r.json()
-    return data["total"]
+async def get_twitch_follows(twitch, numeric_user_id):
+    r = await twitch.get_channel_followers(numeric_user_id)
+    return r.total
