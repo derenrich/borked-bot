3efeda5e1e47c7e500bf10053167a01cb58ef6eb
DErenrich <daniel@erenrich.net>
2025-11-23 04:15:58 -0800

    setup a venv first

--- run.sh
+++ run.sh
@@ -3,7 +3,11 @@
 # wrapper script to launch run.py with the correct environment and passed arguments
 # meant for use in toolforge
 
-python3 -m pip install --user uv
+
+python3 -m venv ~/venv
+source ~/venv/bin/activate
+pip install -U pip wheel
+pip install --user uv
 
 uv run "$@"
 
